72c456e6a5e3b309c2c23f6daf7216fb9db19270
Null-ch <zuxobot@yandex.ru>
2023-06-07 16:49:46 +0300

    add mail with pass & verify by email

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Str;
+use App\Mail\User\PasswordMail;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\Admin\User\StroreRequest;
 
 class StoreController extends Controller
@@ -12,8 +16,11 @@ class StoreController extends Controller
     public function __invoke(StroreRequest $request)
     {
         $data = $request->validated();
-        $data['password'] = Hash::make($data['password']);
-        User::firstOrCreate(['email' => $data['email']],$data);
+        $password = Str::random(10);
+        $data['password'] = Hash::make($password);
+        $user = User::firstOrCreate(['email' => $data['email']],$data);
+        Mail::to($data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
         return redirect()->route('admin.users.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Main\IndexController;
-use App\Http\Controllers\Admin\Main\IndexController as IndexControllerAdmin;
 
 use App\Http\Controllers\Admin\Category\EditController;
 use App\Http\Controllers\Admin\Category\ShowController;
@@ -10,80 +10,84 @@ use App\Http\Controllers\Admin\Category\StoreController;
 use App\Http\Controllers\Admin\Category\CreateController;
 use App\Http\Controllers\Admin\Category\DeleteController;
 use App\Http\Controllers\Admin\Category\UpdateController;
-use App\Http\Controllers\Admin\Category\IndexController as IndexControllerCategory;
-
 use App\Http\Controllers\Admin\Tag\EditController as TagEditController;
+
 use App\Http\Controllers\Admin\Tag\ShowController as TagShowController;
+use App\Http\Controllers\Admin\Post\EditController as PostEditController;
+use App\Http\Controllers\Admin\Post\ShowController as PostShowController;
+use App\Http\Controllers\Admin\Tag\IndexController as TagIndexController;
 use App\Http\Controllers\Admin\Tag\StoreController as TagStoreController;
+use App\Http\Controllers\Admin\User\EditController as UserEditController;
+use App\Http\Controllers\Admin\User\ShowController as UserShowController;
+
+use App\Http\Controllers\Admin\Post\IndexController as PostIndexController;
+use App\Http\Controllers\Admin\Post\StoreController as PostStoreController;
 use App\Http\Controllers\Admin\Tag\CreateController as TagCreateController;
 use App\Http\Controllers\Admin\Tag\DeleteController as TagDeleteController;
 use App\Http\Controllers\Admin\Tag\UpdateController as TagUpdateController;
-use App\Http\Controllers\Admin\Tag\IndexController as TagIndexController;
+use App\Http\Controllers\Admin\User\IndexController as UserIndexController;
+use App\Http\Controllers\Admin\User\StoreController as UserStoreController;
 
-use App\Http\Controllers\Admin\Post\EditController as PostEditController;
-use App\Http\Controllers\Admin\Post\ShowController as PostShowController;
-use App\Http\Controllers\Admin\Post\StoreController as PostStoreController;
+use App\Http\Controllers\Admin\Main\IndexController as IndexControllerAdmin;
 use App\Http\Controllers\Admin\Post\CreateController as PostCreateController;
 use App\Http\Controllers\Admin\Post\DeleteController as PostDeleteController;
 use App\Http\Controllers\Admin\Post\UpdateController as PostUpdateController;
-use App\Http\Controllers\Admin\Post\IndexController as PostIndexController;
-
-use App\Http\Controllers\Admin\User\EditController as UserEditController;
-use App\Http\Controllers\Admin\User\ShowController as UserShowController;
-use App\Http\Controllers\Admin\User\StoreController as UserStoreController;
 use App\Http\Controllers\Admin\User\CreateController as UserCreateController;
 use App\Http\Controllers\Admin\User\DeleteController as UserDeleteController;
 use App\Http\Controllers\Admin\User\UpdateController as UserUpdateController;
-use App\Http\Controllers\Admin\User\IndexController as UserIndexController;
+use App\Http\Controllers\Admin\Category\IndexController as IndexControllerCategory;
 
-Route::group(['App\Http\Controllers\Main'], function () {
+Route::group(['App\Http\Controllers\Main', 'middleware' => ['auth']], function () {
     Route::get('/', IndexController::class);
 });
-Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function () {
-        Route::get('/', IndexControllerAdmin::class)->name('admin.index');
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
+    Route::get('/', IndexControllerAdmin::class)->name('admin.index');
 });
-Route::group(['prefix' => 'admin/categories'], function () {
-    Route::get('/', IndexControllerCategory::class)->name('admin.category.index');
-    Route::get('/create', CreateController::class)->name('admin.category.create');
-    Route::post('/', StoreController::class)->name('admin.category.store');
-    Route::get('/{category}', ShowController::class)->name('admin.category.show');
-    Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
-    Route::patch('/{category}', UpdateController::class)->name('admin.category.update');
-    Route::delete('/{category}', DeleteController::class)->name('admin.category.delete');
-});
+Route::middleware(['auth', 'admin', 'verified'])->group(function () {
+    Route::group(['prefix' => 'admin/categories'], function () {
+        Route::get('/', IndexControllerCategory::class)->name('admin.category.index');
+        Route::get('/create', CreateController::class)->name('admin.category.create');
+        Route::post('/', StoreController::class)->name('admin.category.store');
+        Route::get('/{category}', ShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', EditController::class)->name('admin.category.edit');
+        Route::patch('/{category}', UpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', DeleteController::class)->name('admin.category.delete');
+    });
 
-Route::group(['prefix' => 'admin/tag'], function () {
-    Route::get('/', TagIndexController::class)->name('admin.tag.index');
-    Route::get('/create', TagCreateController::class)->name('admin.tag.create');
-    Route::post('/', TagStoreController::class)->name('admin.tag.store');
-    Route::get('/{tag}', TagShowController::class)->name('admin.tag.show');
-    Route::get('/{tag}/edit', TagEditController::class)->name('admin.tag.edit');
-    Route::patch('/{tag}', TagUpdateController::class)->name('admin.tag.update');
-    Route::delete('/{tag}', TagDeleteController::class)->name('admin.tag.delete');
-});
+    Route::group(['prefix' => 'admin/tag'], function () {
+        Route::get('/', TagIndexController::class)->name('admin.tag.index');
+        Route::get('/create', TagCreateController::class)->name('admin.tag.create');
+        Route::post('/', TagStoreController::class)->name('admin.tag.store');
+        Route::get('/{tag}', TagShowController::class)->name('admin.tag.show');
+        Route::get('/{tag}/edit', TagEditController::class)->name('admin.tag.edit');
+        Route::patch('/{tag}', TagUpdateController::class)->name('admin.tag.update');
+        Route::delete('/{tag}', TagDeleteController::class)->name('admin.tag.delete');
+    });
 
-Route::group(['prefix' => 'admin/post'], function () {
-    Route::get('/', PostIndexController::class)->name('admin.posts.index');
-    Route::get('/create', PostCreateController::class)->name('admin.post.create');
-    Route::post('/', PostStoreController::class)->name('admin.post.store');
-    Route::get('/{post}', PostShowController::class)->name('admin.post.show');
-    Route::get('/{post}/edit', PostEditController::class)->name('admin.post.edit');
-    Route::patch('/{post}', PostUpdateController::class)->name('admin.post.update');
-    Route::delete('/{post}', PostDeleteController::class)->name('admin.post.delete');
-});
-
-Route::group(['prefix' => 'admin/user'], function () {
-    Route::get('/', UserIndexController::class)->name('admin.users.index');
-    Route::get('/create', UserCreateController::class)->name('admin.user.create');
-    Route::post('/', UserStoreController::class)->name('admin.user.store');
-    Route::get('/{user}', UserShowController::class)->name('admin.user.show');
-    Route::get('/{user}/edit', UserEditController::class)->name('admin.user.edit');
-    Route::patch('/{user}', UserUpdateController::class)->name('admin.user.update');
-    Route::delete('/{user}', UserDeleteController::class)->name('admin.user.delete');
+    Route::group(['prefix' => 'admin/post'], function () {
+        Route::get('/', PostIndexController::class)->name('admin.posts.index');
+        Route::get('/create', PostCreateController::class)->name('admin.post.create');
+        Route::post('/', PostStoreController::class)->name('admin.post.store');
+        Route::get('/{post}', PostShowController::class)->name('admin.post.show');
+        Route::get('/{post}/edit', PostEditController::class)->name('admin.post.edit');
+        Route::patch('/{post}', PostUpdateController::class)->name('admin.post.update');
+        Route::delete('/{post}', PostDeleteController::class)->name('admin.post.delete');
+    });
+    
+    Route::group(['prefix' => 'admin/user'], function () {
+        Route::get('/', UserIndexController::class)->name('admin.users.index');
+        Route::get('/create', UserCreateController::class)->name('admin.user.create');
+        Route::post('/', UserStoreController::class)->name('admin.user.store');
+        Route::get('/{user}', UserShowController::class)->name('admin.user.show');
+        Route::get('/{user}/edit', UserEditController::class)->name('admin.user.edit');
+        Route::patch('/{user}', UserUpdateController::class)->name('admin.user.update');
+        Route::delete('/{user}', UserDeleteController::class)->name('admin.user.delete');
+    });
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
