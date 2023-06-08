9b797f5dbe6961589aac313c9247903fd72f9a0f
Null-ch <zuxobot@yandex.ru>
2023-06-08 15:11:41 +0300

    Added queues at registration

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -10,17 +10,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\Admin\User\StroreRequest;
+use App\Jobs\StoreUserJob;
 
 class StoreController extends Controller
 {
     public function __invoke(StroreRequest $request)
     {
         $data = $request->validated();
-        $password = Str::random(10);
-        $data['password'] = Hash::make($password);
-        $user = User::firstOrCreate(['email' => $data['email']],$data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        StoreUserJob::dispatch($data);
         return redirect()->route('admin.users.index');
     }
 }
